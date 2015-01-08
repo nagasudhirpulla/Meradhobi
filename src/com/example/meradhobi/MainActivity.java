858ea7b8ee1adb49cccf0624d6a612b7b1096bef
Sudhir <nagasudhirpulla@gmail.com>
2015-01-08 19:43:28 +0530

    Adding Home Icon to Action Bar

--- src/com/example/meradhobi/MainActivity.java
+++ src/com/example/meradhobi/MainActivity.java
@@ -12,6 +12,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getActionBar().setDisplayShowHomeEnabled(true);
+        getActionBar().setIcon(R.drawable.ic_launcher);
     }
 
 
